776f74c372c8775c627b0f1ece32343647b15c3a
passwd <kevin@cheese.is>
2016-07-27 09:28:46 -0700

    change config file name remove double file check in installer

--- install.sh
+++ install.sh
@@ -3,11 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f $HOME/.jenkins-config ]; then
-  cp $DIR/config-sample.sh $HOME/.jenkins-config
-fi
-
-if [ ! -f /usr/local/bin/jenkins ]; then
-  cp $DIR/config-sample.sh $HOME/.jenkins-config
+  cp $DIR/config-sample $HOME/.jenkins-config
 fi
 
 touch /etc/jenkins
@@ -20,4 +16,4 @@ else
   echo 'Could not install script'
 fi
 
-exit;
+exit
